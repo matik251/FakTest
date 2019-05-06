bd6e0b886a2ead4f8e07e891609d5bf10af39031
Matik251 <mateusz.pawel.kula@gmail.com>
2019-05-06 16:31:15 +0200

    Initial commit

---
+++ FakTest/Sprzedaż.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace FakTest
+{
+    /// <summary>
+    /// Interaction logic for Sprzedaż.xaml
+    /// </summary>
+    public partial class Sprzedaż : Window
+    {
+        public Sprzedaż()
+        {
+            InitializeComponent();
+        }
+
+        private void ButtonBase_OnClick(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Nie znaleziono podanego produktu");
+        }
+    }
+}
